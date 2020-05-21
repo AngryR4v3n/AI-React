18084d06e7fc60aaaee125dbfe525cb0131c66b1
Francisco Molina <molinajimenez01@gmail.com>
2020-05-21 13:34:26 +0200

    identified entrance and exit
    fixed eslint as well, should start docummenting this...

--- src/App.js
+++ src/App.js
@@ -13,16 +13,44 @@ export default class App extends React.Component {
       rows: 0,
       sizeCell: 0,
       text: "",
-      special: []
+      special: [],
+      actualMaze: []
     }
+  }
+  transformLocations = (arr) => {
+    for (let i = 0; i < arr.length; i++) {
+      arr[i] = parseInt(arr[i])
+      if (arr[i] === 0) {
+        arr[i] = 1
+      } else {
+        arr[i] = (arr[i] * 2) + 1
+      }
+    }
+    return arr
+  }
+  identifySpecials = (paramArr) => {
+    let entrance = this.state.text[this.state.text.length - 2]
+    let exit = this.state.text[this.state.text.length - 1]
+
+    //we need to split..
+    entrance = entrance.split(',')
+    exit = exit.split(',')
+    entrance = this.transformLocations(entrance)
+    exit = this.transformLocations(exit)
+    //change values of real matrix
+
+    paramArr[entrance[0]][entrance[1]] = 2
+    paramArr[exit[0]][exit[1]] = 9
+
+
   }
   getMaze = () => {
     let arr = new Array(this.state.rows);
-    //init
+    //init temp array for actual maze
     for (let i = 0; i < this.state.text.length; i++) {
       arr[i] = new Array(this.state.cols)
     }
-
+    //we read vertical walls and extend if it finds the pattern in y axis
     for (let j = 1; j < this.state.text[0].length; j++) {
       for (let i = 1; i < this.state.text.length; i++) {
 
@@ -32,20 +60,20 @@ export default class App extends React.Component {
         else if (i + 1 >= this.state.text.length) {
           break
         }
-        else if (this.state.text[i - 1][j] === '|' && this.state.text[i + 1][j] === '|' && this.state.text[i][j] === ' '){
+        else if (this.state.text[i - 1][j] === '|' && this.state.text[i + 1][j] === '|' && this.state.text[i][j] === ' ') {
           arr[i][j] = 1
-        } else{
+        } else {
           arr[i][j] = 0
         }
       }
     }
-
-    for(let i = 1; i<this.state.text.length; i++){
-      for(let j = 1; j<this.state.text[0].length; j++){
-        if(this.state.text[i][j] === '-'){
+    //we read horizontal lines an extend if it finds the pattern in x axis
+    for (let i = 1; i < this.state.text.length; i++) {
+      for (let j = 1; j < this.state.text[0].length; j++) {
+        if (this.state.text[i][j] === '-') {
           arr[i][j] = 1
         }
-        else if(this.state.text[i][j - 1] === '-' && this.state.text[i][j + 1] === '-' && this.state.text[i][j] === ' '){
+        else if (this.state.text[i][j - 1] === '-' && this.state.text[i][j + 1] === '-' && this.state.text[i][j] === ' ') {
           arr[i][j] = 1
         }
       }
@@ -58,12 +86,13 @@ export default class App extends React.Component {
       arr[this.state.rows][i] = 1
       arr[i][this.state.cols] = 1
     }
-
-
-
+    this.identifySpecials(arr)
+    //this will contain the actual maze that the AI will solve.
     this.setState({
       actualMaze: arr
     })
+
+
   }
 
   handleFile = (file) => {
@@ -71,9 +100,9 @@ export default class App extends React.Component {
     fileReader.readAsText(file)
     fileReader.onloadend = (e) => {
       var content = e.target.result
-
       this.setState({
         text: content.split("\n"),
+        // eslint-disable-next-line
         rows: content.match(new RegExp("\n", "g") || []).length - 1,
         cols: content.split('\n').length - 2 //count from 0.
       })
@@ -116,8 +145,6 @@ export default class App extends React.Component {
     for (let i = 1; i < this.state.rows - 1; i++) {
       for (let j = 1; j < this.state.cols - 1; j++) {
         if (this.state.text[i][j] === '|') {
-          //maze[i][j] = '#';
-
           if (j === this.state.cols - 1) {
             continue
           } else {
