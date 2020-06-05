410199d9498126a91b0020b2f895be029697f6c9
Francisco Molina <molinajimenez01@gmail.com>
2020-06-05 11:57:34 +0200

    Updates

---
+++ precache-manifest.d5ccd4a40acad455faa0d6750936a977.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff72a514fd31c21ce6a7d14fef9a937f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "277bfd2daf9bd84936f4",
+    "url": "/static/css/2.d57f275b.chunk.css"
+  },
+  {
+    "revision": "05a087267832ded121a6",
+    "url": "/static/css/main.46a869e1.chunk.css"
+  },
+  {
+    "revision": "277bfd2daf9bd84936f4",
+    "url": "/static/js/2.d12b6fdb.chunk.js"
+  },
+  {
+    "revision": "ebc14457535110648808604cc4b96810",
+    "url": "/static/js/2.d12b6fdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05a087267832ded121a6",
+    "url": "/static/js/main.0469c77d.chunk.js"
+  },
+  {
+    "revision": "b31e2c6bec40f6efcbb0",
+    "url": "/static/js/runtime-main.ee2fae80.js"
+  }
+]);
